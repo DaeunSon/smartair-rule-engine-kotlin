5283f2829fde2a2001e120629fdcd6286220cf1f
DaeunSon <130820902+DaeunSon@users.noreply.github.com>
2025-11-20 15:22:59 +0900

    feat : 이상 결과 모델 추가

--- src/main/kotlin/domain/report/AnomalyResult.kt
+++ src/main/kotlin/domain/report/AnomalyResult.kt
@@ -1,2 +1,10 @@
-package com.daeun.smartair.domain.report 
+package com.daeun.smartair.domain.report
 
+import com.daeun.smartair.domain.data.Pollutant
+
+data class AnomalyResult (
+    val pollutant: Pollutant,
+    val type: AnomalyType,
+    val value: Double,
+    val message: String
+)
