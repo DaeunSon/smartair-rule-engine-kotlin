d0385faca006b5fd17a03e24589c753d2024245e
DaeunSon <130820902+DaeunSon@users.noreply.github.com>
2025-11-20 15:23:10 +0900

    feat : 이상 규칙 인터페이스 추가

--- src/main/kotlin/rule/AnomalyRule.kt
+++ src/main/kotlin/rule/AnomalyRule.kt
@@ -1,2 +1,13 @@
 package com.daeun.smartair.rule
 
+import com.daeun.smartair.domain.data.AirQualityData
+import com.daeun.smartair.domain.report.AnomalyResult
+
+sealed interface AnomalyRule {
+    val name : String
+
+    fun evaluate(
+        current: AirQualityData,
+        history: List<AirQualityData>
+    ): AnomalyResult?
+}
