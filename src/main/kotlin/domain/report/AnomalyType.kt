e97e865240b99b745bd86190e454940ffa5ed1ed
DaeunSon <130820902+DaeunSon@users.noreply.github.com>
2025-11-22 11:54:03 +0900

    refactor : 공기질 수치 급하강 이상 감지 삭제

--- src/main/kotlin/domain/report/AnomalyType.kt
+++ src/main/kotlin/domain/report/AnomalyType.kt
@@ -14,11 +14,6 @@ enum class AnomalyType(
         description = "단기간 내에 오염 물질 수치가 급격히 상승하였습니다."
     ),
 
-    RAPID_DROP(
-        label = "급격한 하락",
-        description = "단기간 내에 오염 물질 수치가 급격히 하락하였습니다."
-    ),
-
     SENSOR_ERROR(
         label = "센서 오류",
         description = "센서에서 비정상적인 데이터가 감지되었습니다. 센서 오류 가능성이 있습니다."
