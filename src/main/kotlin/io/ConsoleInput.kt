3712d0db1594790e8f78ba6c9893ad3f2fdcfa0b
DaeunSon <130820902+DaeunSon@users.noreply.github.com>
2025-11-23 15:02:53 +0900

    refactor : 불필요한 public 선언 제거

--- src/main/kotlin/io/ConsoleInput.kt
+++ src/main/kotlin/io/ConsoleInput.kt
@@ -5,7 +5,7 @@ class ConsoleInput {
     /**
      * 콘솔에서 int 입력을 읽어오는 유틸 함수
      */
-    public fun readIntInput(prompt: String): Int {
+     fun readIntInput(prompt: String): Int {
         while(true){
             print(prompt)
             val line = readLine()
@@ -25,7 +25,7 @@ class ConsoleInput {
     /**
      * 콘솔에서 Double 입력을 읽어오는 유틸 함수
      */
-    public fun readDoubleInput(prompt: String): Double {
+     fun readDoubleInput(prompt: String): Double {
         while (true) {
             print(prompt)
             val line = readLine()
